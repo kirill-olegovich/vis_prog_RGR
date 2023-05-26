53f1d43eb89682cdb3b3eaa280b34e30f92d4e7e
Kirill <verbun236@gmail.com>
2023-05-26 07:21:10 +0700

    circuits: init

---
+++ RGR/Models/Class_Project.cs
@@ -0,0 +1,29 @@
+using DynamicData.Binding;
+using System.Collections.ObjectModel;
+
+namespace RGR.Models
+{
+    public class Class_Project : AbstractNotifyPropertyChanged
+    {
+    	private string nameProject;
+    	private ObservableCollection<Class_Circuit> circuits;
+
+        public Class_Project()
+        {
+        	NameProject = "Untitled";
+        	Circuits = new ObservableCollection<Class_Circuit>();
+        }
+
+        public string NameProject
+        {
+            get => nameProject;
+            set=> SetAndRaise(ref nameProject, value);
+        }
+
+        public ObservableCollection<Class_Circuit> Circuits
+        {
+            get => circuits;
+            set => SetAndRaise(ref circuits, value);
+        }
+    }
+}
