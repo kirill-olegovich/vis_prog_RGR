e0c4f411c1708088d0f85321c0310075e7d88e66
Nikita Patrushev <samfishernik@gmail.com>
2023-05-28 14:19:21 +0700

    updated save_hist

---
+++ RGR/Models/YAML_Saver.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+
+namespace RGR.Models
+{
+    public class YAML_Saver : ICollectionProjectSaver
+    {
+        public void SaveYAML(IEnumerable<Class_Project> collection)
+        {
+            var serializer = new SerializerBuilder()
+                 .WithNamingConvention(CamelCaseNamingConvention.Instance)
+                 .WithTagMapping("!name", typeof(Class_Project))
+                 .WithTagMapping("!path", typeof(Class_Project))
+                 .WithIndentedSequences()
+                 .Build();
+            var yaml = serializer.Serialize(collection);
+            using (StreamWriter writer = new StreamWriter(@"..\..\..\all_proj.yaml", false))
+            {
+                writer.WriteLine(yaml);
+            }
+        }
+    }
+}
--- RGR/ViewModels/ProgrammViewModel.cs
+++ RGR/ViewModels/ProgrammViewModel.cs
@@ -16,6 +16,7 @@ namespace RGR.ViewModels
         // private ObservableCollection<Class_Circuit> circuits;
         private Class_Project project;
         private ObservableCollection<Full_Elements> elementsToDraw;
+        private ObservableCollection<Class_Project> all_projects;
         private Full_Elements selected_element;
 
         public ProgrammViewModel()
@@ -64,6 +65,12 @@ namespace RGR.ViewModels
         {
             get => elementsToDraw;
             set => this.RaiseAndSetIfChanged(ref elementsToDraw, value);
+        }
+
+        public ObservableCollection<Class_Project> All_Projects
+        {
+            get => all_projects;
+            set => this.RaiseAndSetIfChanged(ref all_projects, value);
         }
 
         public Full_Elements Selected_Element
@@ -273,7 +280,9 @@ namespace RGR.ViewModels
         public void SaveCollection(string path)
         {
             var xmlCollectionSaver = new XML_Saver();
+            var projectCollectionSaver = new YAML_Saver();
             xmlCollectionSaver.Save(Project.Circuits[0].Elements, path);
+            projectCollectionSaver.SaveYAML(All_Projects);
         }
 
         public void LoadCollection(string path)
